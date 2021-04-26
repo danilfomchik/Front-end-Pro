1ca61a2c6b1de9ade3d95dc5f3c7560691be7270
Fomenko Daniil <danil.fomchik@gmail.com>
2021-04-26 14:55:03 +0300

    master

--- Fomenko_lab25/js/custom.js
+++ Fomenko_lab25/js/custom.js
@@ -8,7 +8,8 @@ let buttonNext = document.querySelectorAll('.next'),
     activityRange = document.querySelector('#activityRange'),
     expectedResultRange = document.querySelector('#expectedResult'),
     expectedResultImg = document.querySelector('#expectedResultImg');
-
+    activityP = document.querySelector('.activityP');
+    resultP = document.querySelector('.resultP');
 
 //активность
 activityRange.addEventListener('input', changeHumanActivity)
@@ -19,15 +20,19 @@ function changeHumanActivity() {
     switch(activityValue){
         case '1':
             activityImg.src = './pictures/sleeping.png';
+            activityP.textContent = 'Я очень люблю спать, иногда сплю до вечера. Из-за моего сбитого режима я большую часть дня сплю, а ночью активничаю.'
             break;
         case '2':
             activityImg.src = './pictures/running.png';
+            activityP.textContent = 'Я достаточно активный человек, люблю пробежки по утрам, но из-за недостатка свободного времени не так часто это делаю.'
             break;
         case '3':
             activityImg.src = './pictures/sitting.png';
+            activityP.textContent = 'Из-за моей работы у меня большую часть времени сидячий образ жизни. Не могу себя заставить пойти на зарядку.'
             break;
         case '4':
             activityImg.src = './pictures/coperaiting.png';
+            activityP.textContent = 'Катастрофически не хватает времени на занятия спортом!'
             break;
         default:
             activityImg.src = './pictures/sitting.png';
@@ -44,12 +49,15 @@ function expectedHumanResult(){
     switch(expectedResult){
         case '1':
             expectedResultImg.src = './pictures/fat.png'
+            resultP.textContent = 'Похудение';
             break;
         case '2':
             expectedResultImg.src = './pictures/skinny.png'
+            resultP.textContent = 'Поддержание текущего веса';
             break;
         case '3':
             expectedResultImg.src = './pictures/athlet.png'
+            resultP.textContent = 'Набор мышечной массы';
             break;
         default:
             expectedResultImg.src = './pictures/skinny.png'
@@ -88,8 +96,9 @@ function goPrevElement(){
         buttonPrev[j].addEventListener('click', function(){
             // Убираем прошлый блок
             sections[pointer].classList.remove('active_block');
-            if(pointer - 1 >= sections.length)
-              pointer = 0;
+            if(pointer - 1 <= -sections.length){
+                pointer = 0;
+            }
             else pointer--;
             
             // Отмечаем новый блок
