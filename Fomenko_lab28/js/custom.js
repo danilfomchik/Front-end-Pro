ce2a4348bbb13fe2b26b86c3fb2db1f68af427ee
Fomenko Daniil <danil.fomchik@gmail.com>
2021-05-08 15:37:21 +0300

    master

---
+++ Fomenko_lab28/js/custom.js
@@ -0,0 +1,23 @@
+const cart = document.querySelector('.cart');
+const cartNumbItem = document.querySelectorAll('input');
+
+
+function nextInput(){
+    for(var value of cartNumbItem){
+        value.addEventListener('input', function(){
+            this.value ? this.nextElementSibling.focus() : null
+        });
+        value.addEventListener('keydown', function(event){
+            if(event.keyCode == 8 && this.value.length === 0){
+                this.previousElementSibling.focus();
+            }
+        });
+        value.addEventListener('keypress', function(event){
+            if(event.keyCode < 48 || event.keyCode > 57){
+                event.preventDefault();
+            }
+        });
+    }
+}
+
+nextInput();
