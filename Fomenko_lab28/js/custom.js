080bd6f801a35127ab22ad261cf7307aa8c79d18
Fomenko Daniil <danil.fomchik@gmail.com>
2021-05-15 08:12:46 +0300

    master

--- Fomenko_lab28/js/custom.js
+++ Fomenko_lab28/js/custom.js
@@ -1,26 +1,71 @@
 const cartNumbItem = document.querySelectorAll('input');
+let valuesArray = [];
 
 
-function nextInput(){
+(function fillingCardNumber(){
     for(var iterator of cartNumbItem){
-        iterator.addEventListener('input', function(){
-            if(this.value){
-                this.nextElementSibling.focus()
-            } else{
-                return null;
-            }
-        });
-        iterator.addEventListener('keydown', function(event){
-            if(event.keyCode == 8 && this.value.length === 0){
-                this.previousElementSibling.focus();
-            }
-        });
-        iterator.addEventListener('keypress', function(event){
-            if(event.keyCode < 48 || event.keyCode > 57){
-                event.preventDefault();
-            }
-        });
+        iterator.addEventListener('input', goNextInput);
+        iterator.addEventListener('keydown', goPreviousInput);
+        iterator.addEventListener('keypress', blockLetters);
+    }
+})();
+
+//next input
+function goNextInput(){
+    if(this.value){
+        this.nextElementSibling.focus()
+        valuesArray.push(this.value);
+        var stringifyValues = valuesArray.join('');
+
+        document.cookie = `name=${stringifyValues}`;
+        console.log('stringifyValues-->', stringifyValues);
     }
 }
 
-nextInput();
+//Previous input
+function goPreviousInput(event){
+    if(event.keyCode == 8 && this.value.length === 0){
+        this.previousElementSibling.focus();
+        valuesArray.pop();
+        var stringifyValues = valuesArray.join('');
+
+        document.cookie = `name=${stringifyValues}`;
+        console.log('stringifyValues-->', stringifyValues);
+    }
+}
+
+//pattern
+function blockLetters(event){
+    if(event.keyCode < 48 || event.keyCode > 57){
+        event.preventDefault();
+    }
+}
+
+alert('Наш сайт использует куки. Продолжая работу на этом сайте вы даёте согласие на использование cookie-файлов.')
+
+//get coockie
+var getCookie = function(name) {
+	var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+var cookieData = getCookie('name');
+
+
+(function cookieFill(){
+    var cookieDataArray = cookieData.split('');
+
+    if(cookieData.length >= 12){
+        for(var i = 0; i < cartNumbItem.length; i++){
+            cartNumbItem[i].value = cookieDataArray[i]
+        }
+    }
+})();
+
+(function isCookie(){
+    if (navigator.cookieEnabled === false){
+        console.log("No cookies!");
+    } else{
+        console.log("Cookies is ready!");
+    }    
+})();
